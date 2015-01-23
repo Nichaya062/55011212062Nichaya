15aa4f959924ce5c5a9dc56dcc3788b983be58f3
Nichaya062 <istudents@iClassrooms-iMac-11.local>
2015-01-23 14:22:19 +0700

    Lab2_week1

---
+++ Lab2_week1.playground/section-1.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+
+var array1 = ["abc","bcd","cgf"]
+for (var i = 0 ; i < array1.count;i++) {
+    println(array1[1])
+}
+
+var array2 = ["a","b","c"]
+var str = ""
+for str in array2 {
+    println(str)
+}
