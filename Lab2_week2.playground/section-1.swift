cd9f2f0b4281aa348653cc588b187d1983256713
Nichaya062 <istudents@iClassrooms-iMac-11.local>
2015-01-23 15:57:15 +0700

    Lab2_week2

---
+++ Lab2_week2.playground/section-1.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+
+class Zoo {
+    let animal: [String: String] = ["Panda":"Bamboo", "Monkey":"banana", "Parakeet":"Worm"]
+    let animal1:String
+    
+    init(animal: String){
+        
+        self.animal1 = animal
+    }
+    
+    func Zoofunny() -> String {
+        var anser: String = ""
+        for(key,value) in animal {
+            if(key == animal1){
+                anser = value
+            }
+        }
+        return anser
+    }
+    
+}
+let Zoo1 = Zoo(animal: "Panda")
+Zoo1.Zoofunny()
